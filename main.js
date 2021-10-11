c314d1439227e7d3539c53d6079ca6ec92e9a884
5oN1A <guarino.sonia@gmail.com>
2021-10-11 17:23:11 +0200

    prova

---
+++ main.js
@@ -0,0 +1,10 @@
+const hamburger = document.querySelector(".hamburger-menu");
+
+console.log(hamburger);
+
+hamburger.addEventListener("click", function () {
+
+
+hamburger.classList.add(".hamburger-menu.active");
+
+})
